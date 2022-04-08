b339c28581bae6599bfd5b52adcd56f9ec01cea3
Jonah Beckford <71855677+jonahbeckford@users.noreply.github.com>
2022-04-08 11:56:49 -0700

    Add -deprecated suppression for Cmdliner

--- src/bin/main.ml
+++ src/bin/main.ml
@@ -1,3 +1,8 @@
+(* Cmdliner 1.0 -> 1.1 deprecated a lot of things. But until Cmdliner 1.1
+   is in common use in Opam packages we should provide backwards compatibility.
+   In fact, Diskuv OCaml is not even using Cmdliner 1.1. *)
+[@@@alert "-deprecated"]
+
 open Cmdliner
 
 (* Error handling *)
