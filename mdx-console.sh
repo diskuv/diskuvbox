6ee952703fee3d97ff96cbf2c97894fdb6f69f89
Jonah Beckford <71855677+jonahbeckford@users.noreply.github.com>
2022-10-26 15:56:44 -0700

    Only run mdx when @runmarkdown

---
+++ mdx-console.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+IN=$1
+shift
+
+#   shellcheck disable=SC2016
+sed 's/```console/```sh/g; s#[$] diskuvbox#$ ./diskuvbox.exe#g' "$IN" > "$IN.sh"
+
+#   produce "$IN.sh".corrected
+ocaml-mdx test --force-output "$IN.sh"
+
+#   shellcheck disable=SC2016
+sed 's/```sh/```console/g; s#[$] ./diskuvbox.exe#$ diskuvbox#g' "$IN.sh.corrected" > "$IN.corrected"
